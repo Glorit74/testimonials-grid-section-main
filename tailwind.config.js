1a4ac80695b193a1972faab0a224ce4a7e772fb1
Flora <laszlo.t.flora@gmail.com>
2024-03-31 08:12:25 +0200

    semantic HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
+    colors: {
+      violet: "hsl(263, 55%, 52%)",
+      grayishBlue: "hsl(217, 19%, 35%)",
+      blackishBlue: "hsl(219, 29%, 14%)",
+      White: "hsl(0, 0%, 100%)",
+      gray: "hsl(0, 0%, 81%)",
+      blue: "hsl(210, 46%, 95%)",
+    },
+	fontFamily : {
+		serif: ['Barlow', serif]
+	},
     extend: {},
   },
   plugins: [],
